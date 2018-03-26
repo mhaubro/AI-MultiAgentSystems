390a6c955f4b016e3256508824e20b73cc097366
cenh <cralle@outlook.com>
2018-03-26 12:44:16 +0200

    Goal constructor without color

--- WarmUp/searchclient/searchClientcpp/Goal.cpp
+++ WarmUp/searchclient/searchClientcpp/Goal.cpp
@@ -3,6 +3,11 @@
 #include <tuple>
 #include <string>
 
+Goal::Goal(char chr, std::tuple<int, int> location){
+    Gchr = chr;
+    Glocation = location;
+}
+
 Goal::Goal(char chr, std::string color, std::tuple<int, int> location){
     Gchr = chr;
     Gcolor = color;
