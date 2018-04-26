26229eaff9c2a060c9198af58104c4d39d544102
cenh <cralle@outlook.com>
2018-04-26 14:51:19 +0200

    Now computes the correct plan

--- Project/searcher/src/MasterSearcher.cpp
+++ Project/searcher/src/MasterSearcher.cpp
@@ -29,21 +29,12 @@ namespace MasterSearcher{
       /*Stores these plans*/
 
       for (int i = 0; i < agents; i++){
-        plans[i] = state->agents[i].search( state);
+        plans[i] = state->agents[i].search(state);
         if (plans[i].size() > longestPlan){
           longestPlan = plans[i].size();
         }
       }
 
-      std::list<Node *> aOnePlan = plans[1];
-      std::cerr << "Printing plan for agent 0\n";
-      std::cerr << "Plan size: " << aOnePlan.size() << "\n";
-
-      int s = aOnePlan.size();
-      for(int i = 0; i < s; i++){
-        std::cerr << aOnePlan.front()->toString();
-        aOnePlan.pop_front();
-      }
       /*Creates a list of strings that is the actual plan*/
       /*Keeps checking for conflicts*/
       std::vector<std::string> plan = std::vector<std::string>();
@@ -51,42 +42,28 @@ namespace MasterSearcher{
       bool isPlanning = true;
       for (int i = 0; i < longestPlan; i++){
         std::string s = "[";
-        for (int j = 0; j < agents-1; j++){
-          if (plans[j].size() > i){
+        for (int j = 0; j < agents; j++){
+          if (plans[j].size() == 0){
             s.append("NoOp");
-            s.append(",");
           }
           /*Checks if we're planning or if there's been a conflict, and if the next move is okay*/
           else if (state->checkAndChangeState(j, plans[j].front()->action) && isPlanning){
             /*If the plans are okay, no conflict */
             s.append(plans[j].front()->action->toString());
-            s.append(",");
             plans[j].pop_front();
           } else {
             isPlanning = false;
             /*Do replanning*/
             s.append("NoOp");
-            s.append(",");
           }
-
-        }
-        /*Taking the last agent, fencepost type problem*/
-        if (plans[agents-1].size() > i){
-          s.append("NoOp");
-          s.append("]");
-        }
-        else if ( state->checkAndChangeState(agents-1, plans[agents-1].front()->action) && isPlanning){
-          //Handles fencepost, as only the elements must be ,-separated
-          s.append(plans[agents-1].front()->action->toString());
-          s.append("]");
-          plans[agents-1].pop_front();
-        } else {
-          isPlanning = false;
-          s.append("NoOp");
-          s.append("]");
+          if (j == agents-1){
+            s.append("]");
+          }
+          else{
+            s.append(", ");
+          }
         }
         plan.push_back(s);
-        std::cerr << "Iteration\n";
         if (state->isGoalState()){
           return plan;
         }
